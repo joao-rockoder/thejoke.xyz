081279c6df82526b9b26c9f4dde31d8a8dbe24a9
joao <jonny_ramos@apple.com>
2021-01-14 22:06:01 +0000

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,11 @@
 import React from 'react';
-import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
 	<Layout>
 		<SEO title="Home" />
-		<h1>Hi jonny</h1>
-		<p>Now go build something great.</p>
-		<div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-			<Image />
-		</div>
-
-		<article>
-			<h2>Playground lab</h2>
-			<Link to="/lab/escape-fractal/">Go fractal</Link> <br />
-		</article>
 	</Layout>
 );
 
