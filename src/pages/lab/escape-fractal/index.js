3acd2eca918331bea4411ae39da9ba412e035ff5
joao <jonny_ramos@apple.com>
2021-01-14 21:42:55 +0000

    fixed lab fractal for server side rendering

--- src/pages/lab/escape-fractal/index.js
+++ src/pages/lab/escape-fractal/index.js
@@ -1,24 +1,3 @@
 /* eslint-disable */
-import React, { useRef, useEffect } from 'react';
-
-import { init } from './model';
-
-import './reset.css';
-
-const Fractal = () => {
-	const ref = useRef(null);
-
-	useEffect(() => {
-		const dispose = init(ref.current)();
-		return dispose;
-	});
-	return (
-		<div ref={ref}>
-			<a href="/" style={{ position: 'fixed', bottom: 0, margin: 20 }}>
-				Back
-			</a>
-		</div>
-	);
-};
-
-export default Fractal;
+import fractal from 'lab/escape-fractal';
+export default fractal;
